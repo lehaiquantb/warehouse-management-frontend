304bc16fe8bcd34f2fc9c25b275790322d5ec52b
lehaiquantb <lehaiquantb@gmail.com>
2020-12-15 01:05:24 +0700

    f

---
+++ src/helpers/index.js
@@ -0,0 +1,15 @@
+import store from "../redux/index";
+
+export const viewMore = () => {
+  console.log("implement feature here");
+};
+
+export const initModule = async (path, module) => {
+  const [reducer, saga] = await Promise.all([
+    import(`../pages/${path}/reducer`),
+    import(`../pages/${path}/saga`),
+  ]);
+  store.injectReducer(module, reducer.default);
+  store.injectSaga(module, saga.default);
+  return "ok";
+};
---
+++ src/pages/routes.js
@@ -0,0 +1,6 @@
+let routes = [];
+const context = require.context(".", true, /route.js$/);
+context.keys().forEach((path) => {
+  routes.push(context(`${path}`).default);
+});
+export default routes;
